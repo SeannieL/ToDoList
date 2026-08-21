f600790fbab7e65c9d00536218d23020b725fb7e
Sean Lim <seanlim@Seans-MacBook-Pro.local>
2026-08-21 22:18:02 +1000

    Added get method for specific id

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require('express');
+import db from './db.js';
+import express from 'express';
+
 const app = express();
 const PORT = 3000;
 
@@ -6,9 +8,32 @@ const PORT = 3000;
 app.use(express.json());
 
 //Get all tasks
-app.get('/api/tasks', (req, res) => {
-    res.send('Hello world');
+app.get('/api/tasks', async (req, res) => {
+   try {
+    const tasks = await db.query('SELECT * FROM tasks');
+    res.status(200).json(tasks.rows);
+   } catch (err){
+    console.error(err);
+    res.status(500).json({error: 'Server error occured while fetching data.'});
+   }
 })
+
+app.get('/api/tasks/:id', async (req,res) => {
+    try{
+        const userId = req.params.id
+        console.log(userId)
+        const query = 'SELECT * from tasks WHERE id = $1';
+        const result = await db.query(query, [userId]);
+
+        if(result.rows.length === 0){
+            return res.status(404).json({error: 'User not found'});
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+} )
 
 app.listen(PORT, function (err){
     if (err) console.log(err);
