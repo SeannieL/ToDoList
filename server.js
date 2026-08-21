6d186354aa7f95313bbeeaeedfbfe83275ad6ac4
Sean Lim <seanlim@Seans-MacBook-Pro.local>
2026-08-21 23:29:36 +1000

    Added delete method for backend

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.get('/api/tasks', async (req, res) => {
 
 app.get('/api/tasks/:id', async (req,res) => {
     try{
-        const userId = req.params.id
+        const { id } = req.params;
         console.log(userId)
         const query = 'SELECT * from tasks WHERE id = $1';
-        const result = await db.query(query, [userId]);
+        const result = await db.query(query, [id]);
 
         if(result.rows.length === 0){
             return res.status(404).json({error: 'User not found'});
@@ -31,9 +31,27 @@ app.get('/api/tasks/:id', async (req,res) => {
         res.json(result.rows[0]);
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server error');
+        res.status(500).json({error : 'Internal Server Error'});
     }
-} )
+})
+
+app.delete('/api/tasks/:id', async (req,res) => {
+    const { id } = req.params;
+
+    try{
+        //Query returns the affect row
+        const query = 'DELETE FROM tasks WHERE id = $1 RETURNING *';
+        const result = await db.query(query, [id]);
+        
+        if(result.rowCount === 0){
+            return res.status(404).json({ message : 'User not found'});
+        }
+
+        res.status(200).json({ message: 'User successfully deleted'})
+    } catch (error){
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+})
 
 app.listen(PORT, function (err){
     if (err) console.log(err);
