08fa273f89be3f51ebc6fcb62d549458c0eed74c
Sean Lim <seanlim@Seans-MacBook-Pro.local>
2026-08-23 22:09:53 +1000

    Added cors to connect to FE

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-import db from './src/config/db.js';
 import router from './src/routes/tasks.routes.js';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+const corsOptions = {
+origin: 'http://localhost:5173', // Allow only your React app's origin
+  methods: 'GET,POST,PUT,DELETE',  // Explicitly allow these HTTP methods
+  credentials: true                // Allow cookies/auth headers if needed
+};
+
+
+//Global cors
+app.use(cors(corsOptions));
 
 //Parse JSON bodies (as sent by API clients) -> instead of using data/end chunk-collecting
 app.use(express.json());
