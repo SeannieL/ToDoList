1cc896c702658d7de579abfecbb22d3547e70b7a
Sean Lim <seanlim@Seans-MacBook-Pro.local>
2026-08-23 21:11:58 +1000

    Finished Handler and Routes for all endpoints

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -11,3 +11,74 @@ export async function getAllTasksHandler(req, res){
         res.status(500).json({error : 'Internal Server Error'});
     }
 }
+
+export async function getTaskByIdHandler(req, res){
+    try{
+        const { id } = req.params;
+        const result = await getTaskById(id);
+         if(result.rowCount === 0){
+            return res.status(404).json({error: 'Task not found'});
+        }
+        res.json(result);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({error : 'Internal Server Error'});
+    }
+}
+
+export async function createTaskHandler(req, res){
+    const { title } = req.body;
+
+    if(!title){
+        return res.status(400).json({ error: 'Task name is required' });
+    }
+
+    try{
+        const result = await createTask(title);
+        res.status(201).json({
+            message: 'Task created successfully',
+            task : result
+        })
+    }catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: 'Server error occurred while inserting data' });
+    }
+}
+
+export async function deleteTaskHandler(req, res){
+    const { id } = req.params;
+
+    try{
+        const result = await deleteTask(id);
+        if(result.rowCount === 0){
+            return res.status(404).json({ message : 'Task not found'});
+        }
+
+        res.status(200).json({ message: 'Task successfully deleted'})
+    } catch (error){
+        return res.status(500).json({ error: 'Internal server error.' });
+    }
+}
+
+export async function updateTaskHandler(req, res){
+    const { id } = req.params;
+    const updates = req.body;  
+
+    if(Object.keys(updates).length === 0 ){
+        return res.status(400).json({ error: 'No fields provided for update.'});
+    }
+
+    try{
+        const result = await updateTask(id, updates);
+
+         if (result.rowCount === 0 ){
+            return res.status(404).json({ error: 'Task not found.'});
+        }
+
+        res.status(200).json(result);
+    }catch (error) {
+
+        console.error('Database error:', error);
+        return res.status(500).json({ error: 'Internal server error.' });
+    }
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllTasksHandler } from "../controllers/tasks.controller.js";
+import { getAllTasksHandler, getTaskByIdHandler , createTaskHandler, deleteTaskHandler, updateTaskHandler} from "../controllers/tasks.controller.js";
 
 const router = express.Router();
 
 
 router.get('/', getAllTasksHandler);
+router.get('/:id', getTaskByIdHandler);
+router.post('/', createTaskHandler);
+router.delete('/:id', deleteTaskHandler);
+router.patch('/:id', updateTaskHandler)
 
 export default router
